ef1bea53568260b5108161a282f5e00fa30e8d76
Qingqing <17600969308@163.com>
2018-08-02 16:47:47 +0800

    first commit

---
+++ app/Http/Controllers/Cms.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Cms extends Controller{
+    //
+    public function test(){
+      return "我是彭晴晴";
+    }
+
+    public function add(){
+      return "";
+    }
+}
